584e3beed5e1f1d9b3dee36b9bc3a4f3787d531d
Andrew <andrew.k.jordan@gmail.com>
2014-02-17 15:11:33 -0800

    cleans up app.rb removes unused helper funcitons

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,5 @@
 require 'sinatra'
 require 'extensions/get_or_post'
-require 'helpers/log_request_params'
 
 #load config
 config_file 'config.yml'
@@ -8,27 +7,24 @@ config_file 'config.yml'
 #Initialize Mixpanel Tracker with project_token stored in config.yml
 tracker = Mixpanel::Tracker.new(settings.mixpanel_project_token)
 
-#You should point the VoiceURL of your Twilio number to www.appdomain.com/
+#Point the VoiceURL of your Twilio number to www.appdomain.com/
 get_or_post("/") do
 
-  # build up a TwiML response using the twilio-ruby library
+  #build up a TwiML response using the twilio-ruby library
   response = Twilio::TwiML::Response.new do |r|
     r.Dial :record => "true", :action => "/callback", :timeout => "10" do |d|
-    #If you want to use a whisper, uncomment this line and delete the other
-    #d.Number settings.agent_number, :url => "/whisper"
-      d.Number settings.agent_number
+    #If you want to use a whisper, remove the comment below
+      d.Number settings.agent_number#, :url => "/whisper"
     end
   end
 
-  # print the result to render the TwiML
+  #Return TwiML
   return response.text
 
 end
 
-#This route captures the post from the action callback and updates Mixpanel
 get_or_post("/callback") do
 
-  #This logs every incoming param from the Twilio request into a Mixpanel tracker
   if params['CallSid'] != ''
     tracker.track(params['CallSid'], 'Incoming Call', { 'AccountSid' => params['AccountSid'], 'From' => params['From'], 'To' => params['To'],
       'CallStatus' => params['CallStatus'], 'ApiVersion' => params['ApiVersion'], 'Direction' => params['Direction'], 
--- config.ru
+++ config.ru
@@ -3,8 +3,5 @@ $:.unshift File.dirname(__FILE__)
 require 'bundler'
 Bundler.require
 
-log = File.new("logs/sinatra.log", "a+")
-$stdout.reopen(log)
-
 require 'app/app.rb'
 run Sinatra::Application
